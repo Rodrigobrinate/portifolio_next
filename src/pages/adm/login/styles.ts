9985d84a15736045684986eca89de0753dcef3a3
Rodrigobrinate <rbp20199@gmail.com>
2023-06-10 10:55:01 -0300

    design refatored

---
+++ src/pages/adm/login/styles.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        
+      const response = await axios.post('/api/login', {
+        username: username,
+        password: password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
